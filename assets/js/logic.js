cdfec20e5d1d2ab0df0e4bab360f85149d4fac9f
bmw-dev0p <bmw.dev0p@gmail.com>
2024-07-06 14:59:07 -0400

    added changes rough draft4 07/06

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,4 +1,33 @@
 // TODO: Create logic to toggle the light/dark mode styles for the page and circle. The mode should be saved to local storage.
+const themeToggle = document.querySelector('#toggle');
+const content = document.querySelector('.content');
+// const main = document.querySelector('main');
+// const foot = document.querySelector('footer');
+
+let theme = localStorage.getItem('theme');
+
+themeToggle.addEventListener('click', function () {
+  console.log("toggle clicked");
+  // console.log(content);
+
+  if (theme === 'dark') {
+    theme = 'light';
+    content.setAttribute('class', 'light');
+    // main.setAttribute('class', 'light');
+    // foot.setAttribute('class', 'light');
+  }
+  // If mode is light, apply dark background
+  else {
+    theme = 'dark';
+    content.setAttribute('class', 'dark');
+    // head.setAttribute('class', 'dark');
+    // head.setAttribute('class', 'dark');
+  }  
+  
+  localStorage.setItem('theme', theme);
+  console.log(theme)
+
+});
 
 
 // TODO: Create a function called `readLocalStorage` that reads from local storage and returns the data. If no data exists, return an empty array.
